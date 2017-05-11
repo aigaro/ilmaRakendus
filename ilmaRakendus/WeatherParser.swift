851db9eff8d3c2de560266844ec9e11873fea7d0
Aigar Ottas <aigarottas@aigars-macbook-pro.local>
2017-05-11 14:09:16 +0300

    dates ok!

--- ilmaRakendus/WeatherParser.swift
+++ ilmaRakendus/WeatherParser.swift
@@ -30,7 +30,7 @@ class WeatherParser {
         weather.tempMinNight = Int(xmlData["forecasts"]["forecast"][date]["night"]["tempmin"].element!.text!)!
         weather.tempMaxNight = Int(xmlData["forecasts"]["forecast"][date]["night"]["tempmax"].element!.text!)!
         weather.weatherTextNight = xmlData["forecasts"]["forecast"][date]["night"]["text"].element!.text!
-
+        
         weather.weatherPlacePhenomenonDay = xmlData["forecasts"]["forecast"][0]["day"]["place"][weatherPlace]["phenomenon"].element!.text!
         weather.weatherPlacePhenomenonNight = xmlData["forecasts"]["forecast"][0]["night"]["place"][weatherPlace]["phenomenon"].element!.text!
         weather.weatherPlaceTemperatureNight = Int(xmlData["forecasts"]["forecast"][0]["night"]["place"][weatherPlace]["tempmin"].element!.text!)!
@@ -47,43 +47,23 @@ class WeatherParser {
         for i in 0...3 {
             weather.chosenDate.append(xmlData["forecasts"]["forecast"][i].element!.attribute(by: "date")!.text)
         }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        let date1 = formatter.date(from: weather.chosenDate[0])
+        let date2 = formatter.date(from: weather.chosenDate[1])
+        let date3 = formatter.date(from: weather.chosenDate[2])
+        let date4 = formatter.date(from: weather.chosenDate[3])
+        formatter.dateStyle = .medium
+        let date1a = formatter.string(from: date1!)
+        let date2a = formatter.string(from: date2!)
+        let date3a = formatter.string(from: date3!)
+        let date4a = formatter.string(from: date4!)
+        
+        weather.chosenDate = [date1a, date2a, date3a, date4a]
         completion(weather)
     }
-//    
-//    func parseData(completion: @escaping (WeatherData?) ->  Void) {
-//        Alamofire.request("https://www.ilmateenistus.ee/ilma_andmed/xml/forecast.php").responseString {
-//            response in
-//            print("Success: \(response.result.isSuccess)")
-//            print("Fail: \(response.result.isFailure)")
-//            guard let data = response.result.value else {
-//                completion(nil)
-//                return
-//            }
-//            if response.result.isSuccess == true {
-//                let xmlData = SWXMLHash.lazy(data)
-//                var weather = WeatherData()
-//                
-//                weather.tempMinDay = Int(xmlData["forecasts"]["forecast"][date]["day"]["tempmin"].element!.text!)!
-//                weather.tempMaxDay = Int(xmlData["forecasts"]["forecast"][date]["day"]["tempmax"].element!.text!)!
-//                weather.weatherTextDay = xmlData["forecasts"]["forecast"][date]["day"]["text"].element!.text!
-//                weather.tempMinNight = Int(xmlData["forecasts"]["forecast"][date]["night"]["tempmin"].element!.text!)!
-//                weather.tempMaxNight = Int(xmlData["forecasts"]["forecast"][date]["night"]["tempmax"].element!.text!)!
-//                weather.weatherTextNight = xmlData["forecasts"]["forecast"][date]["night"]["text"].element!.text!
-//                weather.weatherPlacePhenomenonDay = xmlData["forecasts"]["forecast"][0]["day"]["place"][weatherPlace]["phenomenon"].element!.text!
-//                weather.weatherPlacePhenomenonNight = xmlData["forecasts"]["forecast"][0]["night"]["place"][weatherPlace]["phenomenon"].element!.text!
-//                weather.weatherPlaceTemperatureNight = Int(xmlData["forecasts"]["forecast"][0]["night"]["place"][weatherPlace]["tempmin"].element!.text!)!
-//                weather.weatherPlaceTemperatureDay = Int(xmlData["forecasts"]["forecast"][0]["day"]["place"][weatherPlace]["tempmax"].element!.text!)!
-//                for i in 0...3 {
-//                    weather.chosenDate.append(xmlData["forecasts"]["forecast"][i].element!.attribute(by: "date")!.text)
-//                }
-//                completion(weather)
-//            } else
-//            {
-//                completion(nil)
-//            }
-//        }
-//        
-//    }
     
 }
 
